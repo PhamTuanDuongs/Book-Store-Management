97f90945b6e4a376daeaa139007a54bb26652a31
1st251 <120216010+1st251@users.noreply.github.com>
2023-04-24 20:54:17 +0700

    Update App.js

--- react-app/book-store/src/App.js
+++ react-app/book-store/src/App.js
@@ -39,7 +39,6 @@ function App() {
         <Route path="/home" element={<Home />}></Route>
         <Route path="/listBook" element={<PrivateRoute path="/listBook" element={<ListBook handleLogout={handleLogout} />} />} />
         <Route path="/listBookByUser" element={<ListBookByUser />}></Route>
-        <Route path="/listBookByCategory" element={<ListBookByCategory />}></Route>
         <Route path="/category" element={<ListCategory />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
         <Route path="/login" element={<Login />}></Route>
