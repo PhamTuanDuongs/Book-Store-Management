7162753b81e7db188368c55e32b407765bfef880
phucvux <phucvux1804@gmail.com>
2023-04-25 19:20:47 +0700

    filter book by title

--- react-app/book-store/src/components/ListBook.js
+++ react-app/book-store/src/components/ListBook.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const ListBook = () => {
   const [loading, setLoading] = useState('');
   const [books, setBooks] = useState([]);
+  const [search, setSearch] = useState('');
   var pageView = sessionStorage.getItem("pageView");
   useEffect(() => {
     const fetchData = async () => {
@@ -19,16 +20,28 @@ const ListBook = () => {
     fetchData();
 
   }, []);
-
+  console.log(search)
   return (
     <>
 
       <div className="container mx-auto px-4 mt-10">
-        {pageView}
+<form class="flex items-center">   
+    <label for="simple-search" class="sr-only">Search</label>
+    <div class="relative w-full">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+        </div>
+        <input onChange={(e) => setSearch(e.target.value)} type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required/>
+    </div>
+    
+</form>
+
         {!loading && (
           <div className="grid grid-cols-6 gap-6 justify-evenly">
-            {books.map((book) => (
-              <div className="max-width: 144px">
+            {books.filter((book) => {
+              return search.toLowerCase() === '' ? book : book.title.toLowerCase().includes(search);
+            }).map((book) => (
+              <div className="max-width: 144px" key={book.id}>
                 <a href="https://www.w3schools.com?">
                 <div className="content-center">
                 <img style={{width: "144px",height: "200px"}} src={"images/" + book.coverPath} alt="Girl in a jacket" />
